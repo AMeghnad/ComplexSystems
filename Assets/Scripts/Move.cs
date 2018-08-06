030d91ea762fd0a757baa3eeec4febbd4d36f5cc
AMeghnad <anshumanmeghnad@gmail.com>
2018-08-06 15:55:56 +1000

    Networking, movement and cam orbit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Move : MonoBehaviour
+public class Move : NetworkBehaviour
 {
-    public float speed = 5f;
+    public float speed = 50f;
     public Rigidbody rigid;
-   
+    public GameObject cam;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            cam.SetActive(false);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float inputH = Input.GetAxis("Horizontal");
-        float inputV = Input.GetAxis("Vertical");
-        Vector3 force = new Vector3(inputH, 0, inputV);
-        rigid.AddForce(force * speed);
+        if (isLocalPlayer)
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+            Vector3 force = new Vector3(inputH, 0, inputV);
+            force = transform.TransformDirection(force);
+            rigid.AddForce(force * speed);
+        }
     }
 }
