8dd28bcea226c3b92c01f0709fb5f57b0b48c0e1
AMeghnad <anshumanmeghnad@gmail.com>
2018-11-12 15:59:26 +1100

    GUI Settings

---
+++ Assets/GoHome/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoHome
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed = 20f;
+        public float maxVelocity = 20f;
+        public Rigidbody rigid;
+
+        //Constrain the velocity per update
+        void Update()
+        {
+            //Vector3 vel = rigid.velocity;
+            //if (Mathf.Abs(vel.x) > maxVelocity)
+            //{
+            //    vel.x = vel.normalized.x * maxVelocity;
+            //}
+            //if (Mathf.Abs(vel.z) > maxVelocity)
+            //{
+            //    vel.z = vel.normalized.z * maxVelocity;
+            //}
+            //rigid.velocity = vel;
+        }
+
+        // Collect item on trigger enter
+        void OnTriggerEnter(Collider other)
+        {
+
+        }
+        // Input method for movement
+        public void Move(float inputH, float inputV)
+        {
+            //Generate direction from input (horizontal & vertical)
+            Vector3 direction = new Vector3(inputH, 0, inputV);
+
+            Vector3 euler = Camera.main.transform.eulerAngles;
+            direction = Quaternion.Euler(new Vector3(0, euler.y, 0)) * direction;
+
+            // Set velocity to direction (not including y axis)
+            Vector3 vel = rigid.velocity;
+            vel.x = direction.x * speed;
+            vel.z = direction.z * speed;
+            // Apply velocity to rigidbody
+            rigid.velocity = vel;
+        }
+    }
+}
---
+++ Assets/GoHome/Scripts/GameManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using System.Xml.Serialization;
+using System.IO;
+
+namespace GoHome
+{
+
+    // TASK: Save this data using JSON / XML
+    [System.Serializable] // Means it can be converted to another format
+    public class GameData
+    {
+        public int score;
+        public int level;
+    }
+    public class GameManager : MonoBehaviour
+    {
+        #region Singleton
+        public static GameManager Instance;
+        void Awake()
+        {
+            Instance = this;
+        }
+        void OnDestroy()
+        {
+            Save();
+            Instance = null;
+        }
+        #endregion
+        public GameData data = new GameData();
+        public string fileName = "GameSave";
+
+        public int currentScore = 0;
+        public int currentLevel = 0;
+        public bool isGameRunning = true;
+        public Transform levelContainer;
+
+
+        private Level[] levels;
+
+        private void Start()
+        {
+            Load();
+            // Get all levels within level container
+            levels = levelContainer.GetComponentsInChildren<Level>();
+            // Set level to current
+            SetLevel(currentLevel);
+
+        }
+
+        public void GameOver()
+        {
+
+        }
+
+        public void EndGame()
+        {
+
+        }
+
+        public void Save()
+        {
+            data.score = currentScore;
+            data.level = currentLevel;
+            string fullPath = Application.dataPath + "/GoHome/Data/" + fileName + ".json";
+            string json = JsonUtility.ToJson(data);
+            File.WriteAllText(fullPath, json);
+
+            print("Saved to path" + fullPath);
+        }
+
+        void Load()
+        {
+            string fullPath = Application.dataPath + "/GoHome/Data/" + fileName + ".json";
+            string json = File.ReadAllText(fullPath);
+            data = JsonUtility.FromJson<GameData>(json);
+
+            print("Loaded from path: " + fullPath);
+
+            currentScore = data.score;
+            currentLevel = data.level;
+        }
+
+        // Disables all levels except the levelIndex
+        public void SetLevel(int levelIndex)
+        {
+            // Loop through all levels
+            for (int i = 0; i < levels.Length; i++)
+            {
+                GameObject level = levels[i].gameObject;
+                level.SetActive(false); // Disable level
+                // Is current index (i) the same as levelIndex?
+                if (i == levelIndex)
+                {
+                    //Enable that level instead
+                    level.SetActive(true);
+                }
+            }
+        }
+
+        // Switches to the next level when called
+        public void NextLevel()
+        {
+            // Increase currentlevel
+            currentLevel++;
+            // If currentLevel exceeds level length
+            if (currentLevel >= levels.Length)
+            {
+                // GameOver
+
+            }
+            else
+            {
+                // Set current level
+                SetLevel(currentLevel);
+            }
+        }
+        public void NewGame()
+        {
+            // Get currentScen
+            Scene currentScene = SceneManager.GetActiveScene();
+            // Reload it
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
